6308b1da82d760150864409dc183edee987f40d9
Leo <hxc20001227@sina.com>
2022-02-15 03:00:38 +0000

    updated login and logout

--- cs310_website/members/urls.py
+++ cs310_website/members/urls.py
@@ -3,7 +3,9 @@ from .import views
 
 
 urlpatterns = [
-    # path('', views.home, name = 'home'),
+    path('login_user', views.login_user, name = 'login'),
+    path('logout_user', views.logout_user, name = 'logout'),
+    path('register_user', views.register_user, name = 'register_user'),
     # path('<int:year>/<str:month>/', views.home, name = 'home'),
     # path('events', views.all_events, name = 'list-events'),
     ]
--- cs310_website/members/views.py
+++ cs310_website/members/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import RegisterUserForm
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.success(request,("Error Login"))
+			return redirect('login')
+	else:
+		return render(request, 'authenticate/login.html', {})
+
+
+def logout_user(request):
+		logout(request)
+		messages.success(request,("Logged out"))
+		return redirect('home')
+
+def register_user(request):
+	if request.method == "POST":
+		form = RegisterUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Regitseration Successful"))
+			return redirect('home')
+	else:
+		form = RegisterUserForm()
+
+	return render(request, 'authenticate/register_user.html',{
+		'form':form,
+		})
+	
